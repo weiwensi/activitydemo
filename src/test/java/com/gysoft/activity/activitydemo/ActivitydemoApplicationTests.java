477a0a733523193a9ebd6646ab61ecf4840ef5dd
17637900215 <wxx521215>
2019-01-03 00:24:43 +0800

    环境

---
+++ src/test/java/com/gysoft/activity/activitydemo/ActivitydemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.gysoft.activity.activitydemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ActivitydemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
